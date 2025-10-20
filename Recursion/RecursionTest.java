79d7decc07ad0b0468a33697afd47bb1d4b7524d
YariMil <ymilakin1@hwemail.com>
2025-10-20 14:42:07 -0700

    Class commit 10/20

---
+++ Recursion/RecursionTest.java
@@ -0,0 +1,24 @@
+public class RecursionTest {
+    public static void main(String[] args) {
+        ListNode a = new ListNode(3);
+        ListNode b = new ListNode(2, a);
+        ListNode c = new ListNode(1, b);
+        Recursion.printListInReverse(c);
+
+        String[][] gridTest = new String[][] {{"a", "a", "a"}, {"v", "a", "v"}, {"a", "a", "v"}}; //{"a, a, a"}, {"v, a, v"}, {"a, v, a"}
+        printGrid(gridTest);
+        Recursion.infect(gridTest, 0, 0);
+        printGrid(gridTest);
+        
+    }
+
+    public static void printGrid(String[][] grid) {
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                System.out.print(grid[r][c]);
+                System.out.print(", ");
+            }
+            System.out.println();
+        }
+    }
+}
