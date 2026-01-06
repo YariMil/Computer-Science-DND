1566bd657cf73dde33b23e183ddf2246278e72f4
YariMil <ymilakin1@hwemail.com>
2026-01-06 09:14:30 -0800

    commit 1/6

---
+++ MyBST/BinaryTester.java
@@ -0,0 +1,9 @@
+public class BinaryTester {
+    public static void main(String[] args) {
+        MyBST<Integer> test = new MyBST<Integer>();
+        test.add(50);
+        test.add(25);
+        test.add(75);
+        System.out.println(test.toString());
+    }
+}
---
+++ MyBST/MyBST.java
@@ -0,0 +1,100 @@
+// Implements a BST with BinaryNode nodes
+
+public class MyBST<E extends Comparable<E>> {
+
+	private BinaryNode<E> root; // holds the root of this BST
+
+	// Constructor: creates an empty BST.
+	public MyBST() {
+		root = null;
+	}
+
+	public BinaryNode<E> getRoot() {
+		return root;
+	}
+
+	public int getHeight() {
+		return root.getHeight();
+	}
+
+	// Returns true if this BST contains value; otherwise returns false.
+	public boolean contains(E value) {
+		return false;
+	}
+
+	// Adds value to this BST, unless this tree already holds value.
+	// Returns true if value has been added; otherwise returns false.
+	public boolean add(E value) {
+		if (root == null) {
+			root = new BinaryNode<E>(value, null, 0);
+		}
+		BinaryNode<E> temp = root;
+		int height = 0;
+		while (true) {
+			if (value.compareTo(temp.getValue()) < 0) {
+				if (temp.getLeft() == null) {
+					BinaryNode<E> addition = new BinaryNode<E>(value, temp, height);
+					temp.setLeft(addition);
+					return true;
+				}
+				temp = temp.getLeft();
+				height++;
+			} else if (value.compareTo(temp.getValue()) > 0) {
+				if (temp.getRight() == null) {
+					BinaryNode<E> addition = new BinaryNode<E>(value, temp, height);
+					temp.setRight(addition);
+					return true;
+				}
+				temp = temp.getRight();
+				height++;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	// Removes value from this BST. Returns true if value has been
+	// found and removed; otherwise returns false.
+	// If removing a node with two children: replace it with the
+	// largest node in the right subtree
+	public boolean remove(E value) {
+		return false;
+	}
+
+	// Returns the minimum in the tree
+	public E min() {
+		BinaryNode<E> temp = root;
+		while (temp.getLeft() != null) {
+			temp = temp.getLeft();
+		}
+		return temp.getValue();
+	}
+
+	// Returns the maximum in the tree.
+	public E max() {
+		BinaryNode<E> temp = root;
+		while (temp.getRight() != null) {
+			temp = temp.getRight();
+		}
+		return temp.getValue();
+	}
+
+	// Returns a bracket-surrounded, comma separated list of the contents of the nodes, in order
+	// e.g. [Apple, Cranberry, Durian, Mango]
+	public String toString() {
+		StringBuilder sb = toStringHelp(new StringBuilder(), root);
+		String str = sb.toString();
+		return str.substring(0, str.length() - 2);
+	}
+
+	public StringBuilder toStringHelp(StringBuilder sb, BinaryNode<E> temp) {
+		if (temp.getLeft() != null) {
+			sb = toStringHelp(sb, temp.getLeft());
+		}
+		sb.append(temp.getValue() + ", ");
+		if (temp.getRight() != null) {
+			sb = toStringHelp(sb, temp.getRight());
+		}
+		return sb;
+	}
+}
