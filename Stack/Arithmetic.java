2155c14862f2fb2420b908bf08ffc801acbce7e6
YariMil <ymilakin1@hwemail.com>
2026-01-23 09:18:37 -0800

    Commit 1/23

--- Stack/Arithmetic.java
+++ Stack/Arithmetic.java
@@ -9,39 +9,75 @@ public class Arithmetic {
 		 * an increasing importance thingy if thing alredy here > thing added: GO
 		 */
 		String stout = convertClassicToStout(exp);
-		return 0;
+		return evaluateStout(stout);
 	}
 
 	// Returns the result of doing operand1 operation operand2,
 	// e.g. operate(5, 2, "-") should return 3
 	public static int operate(int operand1, int operand2, String operation) {
+		if (operation.equals("+")) {
+			return operand1 + operand2;
+		} else if (operation.equals("-")) {
+			return operand1 - operand2;
+		} else if (operation.equals("*")) {
+			return operand1 * operand2;
+		} else if (operation.equals("/")) {
+			return operand1 / operand2;
+		} else if (operation.equals("^")) {
+			return (int) (Math.pow(operand1, operand2));
+		} else if (operation.equals("%")) {
+			return (int) operand1 % operand2;
+		}
 		return 0;
 	}
 
 	// Evaluates a String exp that has an arithmetic expression written in STOUT notation
 	public static int evaluateStout(String exp) {
-		return 0;
+		String[] evaluationsToDo = exp.split(" ");
+		MyStack<Integer> evalStack = new MyStack<Integer>();
+		for (String str : evaluationsToDo) {
+			try {
+				evalStack.push(Integer.parseInt(str));
+			} catch (Exception e) {
+				// A little weird assigning, but the one currently on the stack is our operand 2.
+				// The one below is operand 1.
+				Integer operand2 = evalStack.peek();
+				evalStack.pop();
+				Integer operand1 = evalStack.peek();
+				evalStack.pop();
+				evalStack.push(operate(operand1, operand2, str));
+			}
+		}
+		return evalStack.peek();
 	}
 
 	public static String convertClassicToStout(String exp) {
 		MyStack<String> operatorStack = new MyStack<String>();
 		StringBuilder stoutNotation = new StringBuilder();
 		String[] operations = exp.split(" ");
-		for (String str : operations) {
+		for (int i = 0; i < operations.length; i++) {
 			try {
-				stoutNotation.append(Integer.parseInt(str));
+				stoutNotation.append(Integer.parseInt(operations[i]) + " ");
 			} catch (Exception e) {
 				// We have an operator on our hands
 				// if thing already here >= thing added: GO
 				if (operatorStack.empty()) {
-					operatorStack.push(str);
-				} else {
-					if (compareStackToPush(operatorStack.peek(), str)) {
-						// We shift the stack around
-						stoutNotation.append(operatorStack.peek());
+					operatorStack.push(operations[i]);
+
+				} else if (operations[i].equals(")")) {
+					while (!operatorStack.peek().equals("(")) {
+						stoutNotation.append(operatorStack.peek() + " ");
 						operatorStack.pop();
 					}
-					operatorStack.push(str);
+					operatorStack.pop();
+				} else {
+					if (compareStackToPush(operatorStack.peek(), operations[i])
+							&& !operatorStack.peek().equals("(")) {
+						// We shift the stack around
+						stoutNotation.append(operatorStack.peek() + " ");
+						operatorStack.pop();
+					}
+					operatorStack.push(operations[i]);
 				}
 			}
 		}
@@ -50,6 +86,17 @@ public class Arithmetic {
 			operatorStack.pop();
 		}
 		return stoutNotation.toString();
+	}
+
+	public static String autoFormatSpaces(String str) {
+		StringBuilder autoFormat = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != ' ') {
+				autoFormat.append(str.charAt(i));
+				autoFormat.append(' ');
+			}
+		}
+		return autoFormat.toString();
 	}
 
 	private static boolean compareStackToPush(String stackOperator, String operator2) {
--- Stack/ArithmeticTester.java
+++ Stack/ArithmeticTester.java
@@ -1,7 +1,11 @@
 public class ArithmeticTester {
     public static void main(String[] args) {
-        String classic = "9 + 4 * 3 / 2";
+        String classic = "]";
         String stout = Arithmetic.convertClassicToStout(classic);
         System.out.println(stout);
+        String stoutTest = "3 4 3 - 5 1 - + *";
+        System.out.println(Arithmetic.evaluateStout(stoutTest));
+        System.out.println(Arithmetic.evaluate(classic));
+        System.out.println(Arithmetic.evaluate("15 - 1"));
     }
 }
