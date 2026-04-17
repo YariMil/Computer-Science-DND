fc1447fd64fc86a23490c9da8491ff98de930870
YariMil <ymilakin1@hwemail.com>
2026-04-17 11:15:06 -0700

    Commit 4/17

---
+++ Huffman/HuffmanEncoding.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+public class HuffmanEncoding {
+    public static void countFrequency(String fileName) throws IOException {
+        if (fileName == null) {
+            throw new IllegalArgumentException();
+        }
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        while (br.ready()) {
+            Character c = (char) br.read();
+            if (!map.containsKey(c)) {
+                map.put(c, 0);
+            }
+            map.put(c, map.get(c) + 1);
+        }
+        map.put((char) br.read(), 1);
+        System.out.println(map.toString());
+        br.close();
+    }
+}
---
+++ Huffman/HuffmanTester.java
@@ -0,0 +1,9 @@
+public class HuffmanTester {
+    public static void main(String[] args) {
+        try {
+            HuffmanEncoding.countFrequency("S");
+        } catch (Exception e) {
+
+        }
+    }
+}
