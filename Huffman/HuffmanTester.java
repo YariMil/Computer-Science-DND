2b64d2ddf7ba777ac9299cd946d551f60af2c984
YariMil <ymilakin1@hwemail.com>
2026-05-06 10:33:08 -0700

    Making the tester better

--- Huffman/HuffmanTester.java
+++ Huffman/HuffmanTester.java
@@ -5,15 +5,17 @@ import java.util.HashMap;
 public class HuffmanTester {
     public static void main(String[] args) {
         try {
-            // File has 2 a, 1 b, 3 c, 5 t
-            HuffmanCodeGenerator encoding = new HuffmanCodeGenerator("huffmanTest.txt");
+            // Small huffman test
+            HuffmanCodeGenerator encoding = new HuffmanCodeGenerator("smallHuffmanTest.txt");
+            // Making sure we don't have null stuff in the dictionary
             for (int i = 0; i < 128; i++) {
                 if ((char) (i) == encoding.getFrequency((char) i)) {
                     // System.out.println("WOOO");
                 }
             }
-            encoding.makeCodeFile("huffmanDictionary.txt");
-            BufferedReader br = new BufferedReader(new FileReader("huffmanDictionary.txt"));
+            encoding.makeCodeFile("smallHuffmanDictionary.txt");
+            BufferedReader br = new BufferedReader(new FileReader("smallHuffmanDictionary.txt"));
+            // Making we sure we don't have any weird null lines in the codefile
             for (int i = 0; i < 128; i++) {
                 if (br.readLine() == null) {
                     System.out.println(i);
@@ -21,14 +23,25 @@ public class HuffmanTester {
                 }
             }
             br.close();
-            // // This is the file sorted by frequency
-            HuffmanEncoder encoder = new HuffmanEncoder("huffmanDictionary.txt");
+            // Small file just testing special characters and stuff
+            HuffmanEncoder encoder = new HuffmanEncoder("smallHuffmanDictionary.txt");
             // encoding.printDictionary();
-            encoder.encodeFileToHuffmanCodes("huffmanTest.txt", "huffmanEncoded.txt");
-            HuffmanDecoder decoder = new HuffmanDecoder("huffmanDictionary.txt");
-            decoder.decodeFileFromHuffmanCodes("huffmanEncoded.txt", "huffmanDecoded.txt");
-            encoder.encodeFile("huffmanTest.txt");
-            decoder.decodeFile("huffmanTest.txt.huf");
+            encoder.encodeFileToHuffmanCodes("smallHuffmanTest.txt", "smallHuffmanEncoded.txt");
+            HuffmanDecoder decoder = new HuffmanDecoder("smallHuffmanDictionary.txt");
+            decoder.decodeFileFromHuffmanCodes("smallHuffmanEncoded.txt",
+                    "smallHuffmanDecoded.txt");
+            encoder.encodeFile("smallHuffmanTest.txt");
+            decoder.decodeFile("smallHuffmanTest.txt.huf");
+
+            // Big file - The Great Gatsby 
+            HuffmanCodeGenerator bigEncoding = new HuffmanCodeGenerator("huffmanTest.txt");
+            bigEncoding.makeCodeFile("huffmanDictionary.txt");
+            HuffmanEncoder bigEncoder = new HuffmanEncoder("huffmanDictionary.txt");
+            bigEncoder.encodeFileToHuffmanCodes("huffmanTest.txt", "huffmanEncoded.txt");
+            HuffmanDecoder bigDecoder = new HuffmanDecoder("huffmanDictionary.txt");
+            bigDecoder.decodeFileFromHuffmanCodes("huffmanEncoded.txt", "huffmanDecoded.txt");
+            bigEncoder.encodeFile("huffmanTest.txt");
+            bigDecoder.decodeFile("huffmanTest.txt.huf");
 
         } catch (Exception e) {
             System.out.println(e);
